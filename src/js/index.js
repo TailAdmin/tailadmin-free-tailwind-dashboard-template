50aa8c15fac0f361b040ab29837c0ad505e98c24
neajmorshed0 <neajmorshedimon@gmail.com>
2025-12-30 17:13:09 +0600

    date picker update

--- src/js/index.js
+++ src/js/index.js
@@ -24,7 +24,7 @@ flatpickr(".datepicker", {
   mode: "range",
   static: true,
   monthSelectorType: "static",
-  dateFormat: "M j, Y",
+  dateFormat: "M j",
   defaultDate: [new Date().setDate(new Date().getDate() - 6), new Date()],
   prevArrow:
     '<svg class="stroke-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.25 6L9 12.25L15.25 18.5" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>',
